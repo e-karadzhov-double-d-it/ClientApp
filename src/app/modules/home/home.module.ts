64c7261811f20fba290923a5f77773ae957b58c4
e-karadzhov-double-d-it <e.karadzhov@double-d-it.de>
2023-01-09 16:27:12 +0100

    Added heading-section for home container

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './containers/home/home.component';
+import { HeadingSectionComponent } from './components/heading-section/heading-section.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeadingSectionComponent
   ],
   imports: [
     CommonModule,
